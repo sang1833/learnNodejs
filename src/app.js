5f23bda18a5e8381b853d1bcd3ba751b40b77002
Sang <75106540+sang1833@users.noreply.github.com>
2025-01-29 23:20:24 +0700

    add helmet and compression library

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
 const app = express();
 
 // init middleware
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 // init db
 
 // init routes
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello World" });
+  const strCompress = "Hello Tipjs";
+  return res.status(200).json({
+    message: "Hello World",
+    metadata: strCompress.repeat(1000)
+  });
 });
 
 // handling error
